5305713608adf408bef402d421b3fe48681cc379
Christer <christerforsgren91@gmail.com>
2021-09-01 14:07:25 +0200

    Add header

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Header from './components/Header'
 
-export const App = () => {
+const App = () => {
   return (
-    <View>
-      
+    <View style={styles.screen} >
+      <Header title='Guess a Number'/>
     </View>
   )
 }
 
-export default App
 const styles = StyleSheet.create({
-  
+  screen: {
+    flex: 1,
+  }
 });
+
+export default App
