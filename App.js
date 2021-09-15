ce952ed9346bee227adb71b0779662774f801e7b
Christer <christerforsgren91@gmail.com>
2021-09-15 11:10:54 +0200

    Add fonts to the application

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
 import Header from './components/Header'
 import GameOverScreen from './screens/GameOverScreen'
 import GameScreen from './screens/GameScreen'
 import StartGameScreen from './screens/StartGameScreen'
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
+
 const App = () => {
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console-log(err)}
+      />
+    )
+  }
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber)
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -68,7 +68,7 @@ const StartGameScreen = (props) => {
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game</Text>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <Text style={styles.text} >Select a Number</Text>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -111,6 +111,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10,
+    fontFamily: 'open-sans-bold',
   },
   inputContainer: {
     width: 300,
@@ -134,6 +135,9 @@ const styles = StyleSheet.create({
     marginTop: 20,
     alignItems: 'center',
   },
+  text: {
+    fontFamily: 'open-sans'
+  }
 })
 
 export default StartGameScreen
